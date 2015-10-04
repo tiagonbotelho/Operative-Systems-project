a21ca61678278dec7500dfa4d6b7c78716cbe5c7
Pedro Belém <pbelem@student.dei.uc.pt>
2015-10-04 20:19:18 +0100

    Started the processes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<unistd.h>
+
+void statistics(){
+  while(1){
+    printf("STATISTICS PROCESS");
+  }
+}
+
+void start_statistics(){
+  if(fork()==0){
+    statistics();
+  }
+}
+
+void config(){
+  while(1){
+    printf("CONFIG PROCESS");
+  }
+}
+
+
+void start_config(){
+  if(fork()==0){
+    config();
+  }
+}
 
 int main(int argc, char const *argv[]){
+  start_config();
+  config();
+  request_manager(argc,argv);
   return 0;
 }
+
+
